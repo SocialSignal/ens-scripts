370bd8d1f91d5541182a877857a3a250097d68da
Alexandro T. Netto <alex.t.netto@gmail.com>
2023-09-23 13:26:34 -0400

    chore: add prettier

--- src/set-public-resolver.ts
+++ src/set-public-resolver.ts
@@ -2,10 +2,19 @@ import { ethers } from "ethers";
 
 const registryAbi = ["function setResolver(bytes32,address) external"];
 
-export const setResolver = async (signer: ethers.providers.JsonRpcSigner, name: string, resolver: string = "0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41") => {
-    let namehash = ethers.utils.namehash(name);
-    const registry = new ethers.Contract("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e", registryAbi, signer)
-    console.log(namehash)
-    const res = await registry.setResolver(namehash, resolver)
-    return res
-}
+export const setResolver = async (
+  signer: ethers.providers.JsonRpcSigner,
+  name: string,
+  resolver: string = "0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41",
+) => {
+  let namehash = ethers.utils.namehash(name);
+  const registry = new ethers.Contract(
+    "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
+    registryAbi,
+    signer,
+  );
+
+  console.log(namehash);
+  const res = await registry.setResolver(namehash, resolver);
+  return res;
+};
