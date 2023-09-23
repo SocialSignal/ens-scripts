d4757eea284391ee3ae60bec5dbcf7ac5146e39b
Alexandro T. Netto <alex.t.netto@gmail.com>
2023-09-23 16:55:40 -0400

    feat: set tribe records

--- src/set-tribe-records.ts
+++ src/set-tribe-records.ts
@@ -1,57 +1,47 @@
 import { ethers } from "ethers";
-
-const resolverAbi = [
-  "function setText(bytes32 node, string calldata key, string calldata value) external",
-  "function text(bytes32 node, string calldata key) external returns (string memory)",
-  "function multicall(bytes[] calldata data) external returns(bytes[] memory results)",
-];
+import { resolver } from "./utils/contracts";
 
 interface TribeRecordsInputs {
   name: string;
   description: string;
   avatar: string;
-  smallAvatar: string;
-  contractChainId: number;
+  badge: string;
   contractAddress: string; // chainId/address
+  contractChainId: number;
   values?: string[];
 }
 
-export const setRecords = async (
-  signer: ethers.providers.JsonRpcSigner,
-  name: string,
-  records: TribeRecordsInputs,
-  resolverAddress: string = "0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41",
-) => {
+export const setRecords = async (name: string, records: TribeRecordsInputs) => {
   let node = ethers.utils.namehash(name);
-  const resolver = new ethers.Contract(resolverAddress, resolverAbi, signer);
-  await resolver.multicall(buildMulticallData(node, records));
+  const calldata = buildMulticallData(node, records);
+  const res = await resolver.multicall(calldata);
+  await res.wait();
+  console.log(res);
 };
 
 const buildMulticallData = (node: string, tribeRecords: TribeRecordsInputs) => {
-  const resolverInterface = new ethers.utils.Interface(resolverAbi);
-
   var multicallStack = [
-    resolverInterface.encodeFunctionData("setText", [
+    resolver.interface.encodeFunctionData("setText", [
       node,
       "name",
       tribeRecords.name,
     ]),
-    resolverInterface.encodeFunctionData("setText", [
+    resolver.interface.encodeFunctionData("setText", [
       node,
       "description",
       tribeRecords.description,
     ]),
-    resolverInterface.encodeFunctionData("setText", [
+    resolver.interface.encodeFunctionData("setText", [
       node,
-      "smallAvatar",
-      tribeRecords.smallAvatar,
+      "badge",
+      tribeRecords.badge,
     ]),
-    resolverInterface.encodeFunctionData("setText", [
+    resolver.interface.encodeFunctionData("setText", [
       node,
       "avatar",
       tribeRecords.avatar,
     ]),
-    resolverInterface.encodeFunctionData("setText", [
+    resolver.interface.encodeFunctionData("setText", [
       node,
       "tribeContract",
       `${tribeRecords.contractChainId}/${tribeRecords.contractAddress}`,
@@ -60,11 +50,13 @@ const buildMulticallData = (node: string, tribeRecords: TribeRecordsInputs) => {
 
   if (tribeRecords.values !== undefined && tribeRecords.values.length > 0) {
     multicallStack.push(
-      resolverInterface.encodeFunctionData("setText", [
+      resolver.interface.encodeFunctionData("setText", [
         node,
         "values",
         tribeRecords.values.toString(),
       ]),
     );
   }
+
+  return multicallStack;
 };
